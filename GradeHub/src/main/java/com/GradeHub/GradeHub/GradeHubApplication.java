1a130c93bf1f7e6248613d1118b79893a431fbcc
Pashidu Malshan <88223496+Phantom-LK@users.noreply.github.com>
2024-04-03 22:00:06 +0530

    Inizilize

---
+++ GradeHub/src/main/java/com/GradeHub/GradeHub/GradeHubApplication.java
@@ -0,0 +1,13 @@
+package com.GradeHub.GradeHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradeHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradeHubApplication.class, args);
+	}
+
+}
