7ba5941d0258931c82cc616559521cfc220b1494
Pashidu Malshan <88223496+Phantom-LK@users.noreply.github.com>
2024-04-04 15:50:02 +0530

    Repo

---
+++ GradeHub/src/main/java/com/GradeHub/GradeHub/controller/StudentController.java
@@ -0,0 +1,7 @@
+package com.GradeHub.GradeHub.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class StudentController {
+}
---
+++ GradeHub/src/main/java/com/GradeHub/GradeHub/service/StudentService.java
@@ -0,0 +1,9 @@
+package com.GradeHub.GradeHub.service;
+
+import com.GradeHub.GradeHub.entity.Student;
+
+import java.util.List;
+
+public interface StudentService {
+    List<Student> getAllStudent();
+}
