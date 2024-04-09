2cb4294936fd2a4ecd60f91004b66d535a711f3d
Pashidu Malshan <88223496+Phantom-LK@users.noreply.github.com>
2024-04-09 20:28:33 +0530

    NEW

---
+++ GradeHub/src/main/java/com/gradehub/GradeHubApplication.java
@@ -0,0 +1,13 @@
+package com.gradehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradeHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradeHubApplication.class, args);
+	}
+
+}
---
+++ GradeHub/src/test/java/com/gradehub/GradeHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gradehub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradeHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
