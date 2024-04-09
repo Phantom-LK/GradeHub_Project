d906a67d73375309f1cbf4610e2b870bfa6f6c6a
Pashidu Malshan <88223496+Phantom-LK@users.noreply.github.com>
2024-04-09 20:16:12 +0530

    Delete GradeHub directory

--- GradeHub/src/main/java/com/GradeHub/GradeHub/service/StudentService.java
+++
@@ -1,9 +0,0 @@
-package com.GradeHub.GradeHub.service;
-
-import com.GradeHub.GradeHub.entity.Student;
-
-import java.util.List;
-
-public interface StudentService {
-    List<Student> getAllStudent();
-}
