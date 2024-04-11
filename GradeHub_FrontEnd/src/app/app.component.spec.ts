b05803274fc5534413ba4f528fbec5b5e597c879
Pashidu Malshan <88223496+Phantom-LK@users.noreply.github.com>
2024-04-11 22:19:04 +0530

    big

--- GradeHub_FrontEnd/src/app/app.component.spec.ts
+++ GradeHub_FrontEnd/src/app/app.component.spec.ts
@@ -1,10 +1,16 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [
+        RouterTestingModule
+      ],
+      declarations: [
+        AppComponent
+      ],
     }).compileComponents();
   });
 
@@ -14,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'GradeHub_Angular' title`, () => {
+  it(`should have as title 'GradeHub_FrontEnd'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('GradeHub_Angular');
+    expect(app.title).toEqual('GradeHub_FrontEnd');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, GradeHub_Angular');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, GradeHub_FrontEnd');
   });
 });
