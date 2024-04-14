acd7e4224d3bbf29bc4a0ad0a2b145952de1efc8
Pashidu Malshan <88223496+Phantom-LK@users.noreply.github.com>
2024-04-14 20:44:28 +0530

    Admin Dashbord

--- GradeHub_FrontEnd/src/app/app-routing.module.ts
+++ GradeHub_FrontEnd/src/app/app-routing.module.ts
@@ -3,7 +3,9 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
-  {path:"login", component:LoginComponent}
+  {path:"login", component:LoginComponent}, 
+  {path:"admin", loadChildren:()=>import("./modules/admin/admin.module").then(m => m.AdminModule)},
+  {path:"student",loadChildren:() =>import("./modules/student/student.module").then(m => m.StudentModule)}
 ];
 
 @NgModule({
--- GradeHub_FrontEnd/src/app/modules/admin/admin.module.ts
+++ GradeHub_FrontEnd/src/app/modules/admin/admin.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
+import { DashboardComponent } from './admin-components/dashboard/dashboard.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    DashboardComponent
+  ],
   imports: [
     CommonModule,
     AdminRoutingModule
