df66aaebc0e6f550d8c180b6412f464ad499011e
Pashidu Malshan <88223496+Phantom-LK@users.noreply.github.com>
2024-04-26 01:21:41 +0530

    back

--- GradeHub/src/main/java/com/gradehub/repositories/StudentLeaveRepository.java
+++ GradeHub/src/main/java/com/gradehub/repositories/StudentLeaveRepository.java
@@ -1,4 +1,9 @@
 package com.gradehub.repositories;
 
-public interface StudentLeaveRepository {
+import com.gradehub.entities.StudentLeave;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentLeaveRepository extends JpaRepository<StudentLeave, Long> {
 }
--- GradeHub/src/main/java/com/gradehub/services/student/StudentserviceImpl.java
+++ GradeHub/src/main/java/com/gradehub/services/student/StudentserviceImpl.java
@@ -2,11 +2,15 @@ package com.gradehub.services.student;
 
 import com.gradehub.dto.SingleStudentDto;
 import com.gradehub.dto.StudentLeaveDto;
+import com.gradehub.entities.StudentLeave;
 import com.gradehub.entities.User;
+import com.gradehub.enums.StudentLeaveStatus;
+import com.gradehub.repositories.StudentLeaveRepository;
 import com.gradehub.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.Optional;
 
 @Service
@@ -14,6 +18,8 @@ import java.util.Optional;
 public class StudentserviceImpl implements StudentService {
 
     private final UserRepository userRepository;
+
+    private  final StudentLeaveRepository studentLeaveRepository;
 
     @Override
     public SingleStudentDto getStudentById(Long studentId) {
@@ -25,6 +31,21 @@ public class StudentserviceImpl implements StudentService {
 
     @Override
     public StudentLeaveDto applyLeave(StudentLeaveDto studentLeaveDto) {
+        Optional<User> optionalUser = userRepository.findById(studentLeaveDto.getUserid());
+        if (optionalUser.isPresent()){
+            StudentLeave studentLeave = new StudentLeave();
+            studentLeave.setSubject(studentLeaveDto.getSubject());
+            studentLeave.setBody(studentLeaveDto.getBody());
+            studentLeave.setDate(new Date());
+            studentLeave.setStudentLeaveStatus(StudentLeaveStatus.Pending);
+            studentLeave.setUser(optionalUser.get());
+           StudentLeave SubmittedStudentLeave = studentLeaveRepository.save(studentLeave);
+           StudentLeaveDto studentLeaveDto1 = new StudentLeaveDto();
+           studentLeaveDto1.setId(SubmittedStudentLeave.getId());
+           return studentLeaveDto1;
+
+        }
         return null;
+
     }
 }
