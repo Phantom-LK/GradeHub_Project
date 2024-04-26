8125f6e4631b5bbc7fac0cb189b5a5b3fee20ee9
Pashidu Malshan <166420502+hneusicaaa@users.noreply.github.com>
2024-04-26 17:53:44 +0530

    post

--- GradeHub_FrontEnd/src/app/modules/admin/admin.module.ts
+++ GradeHub_FrontEnd/src/app/modules/admin/admin.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
 import { DashboardComponent } from './admin-components/dashboard/dashboard.component';
+import { PostStudentComponent } from './admin-components/post-student/post-student.component';
 
 
 @NgModule({
   declarations: [
-    DashboardComponent
+    DashboardComponent,
+    PostStudentComponent
   ],
   imports: [
     CommonModule,
