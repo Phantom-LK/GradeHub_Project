ee41455ef70f9835e31bef4ce280bad5852ac230
Pashidu Malshan <166420502+hneusicaaa@users.noreply.github.com>
2024-04-27 10:25:49 +0530

    update

--- GradeHub_FrontEnd/src/app/app-routing.module.ts
+++ GradeHub_FrontEnd/src/app/app-routing.module.ts
@@ -2,15 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { noAuthGuard } from './guards/noAuth-guard/no-auth.guard';
+import { DashboardComponent } from './modules/admin/admin-components/dashboard/dashboard.component';
 
 const routes: Routes = [
   {path:"login", component:LoginComponent, canActivate:[noAuthGuard]}, 
   {path:"admin", loadChildren:()=>import("./modules/admin/admin.module").then(m => m.AdminModule)},
-  {path:"student",loadChildren:() =>import("./modules/student/student.module").then(m => m.StudentModule)}
+  {path:"student",loadChildren:() =>import("./modules/student/student.module").then(m => m.StudentModule)},
+  {path:'',component:DashboardComponent}
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- GradeHub_FrontEnd/src/app/modules/admin/admin-components/update-student/update-student.component.ts
+++ GradeHub_FrontEnd/src/app/modules/admin/admin-components/update-student/update-student.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AdminService } from '../../admin-service/admin.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-update-student',
@@ -6,5 +8,21 @@ import { Component } from '@angular/core';
   styleUrl: './update-student.component.scss'
 })
 export class UpdateStudentComponent {
+
+  studentId: number = this.activatedRoute.snapshot.params['studentId'];
+
+  constructor (private service: AdminService,
+    private activatedRoute: ActivatedRoute){
+
+    }
+    ngOnInit(){
+      this.getStudentById();
+    }
+
+    getStudentById(){
+      this.service.getStudentById(this.studentId).subscribe((res)=>{
+        console.log(res);      })
+    }
+  
 
 }
